8958c47a71e62842f510f3df652dbf0931f02d74
GabrielTavaresB <gabrieltavaresborges@hotmail.com>
2026-09-08 22:11:54 -0300

    initial domain_layer

---
+++ src/YaeaY.DataHub.Domain/Abstraction/Entities/Entity.cs
@@ -0,0 +1,56 @@
+﻿namespace YaeaY.DataHub.Domain.Abstraction.Entities;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; protected set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        if (GetType() != other.GetType())
+        {
+            return false;
+        }
+
+        // Entidades "transientes" (sem Id atribuído) não são consideradas iguais
+        if (Id == Guid.Empty || other.Id == Guid.Empty)
+        {
+            return false;
+        }
+
+        return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Entity);
+
+    public override int GetHashCode()
+    {
+        // Para entidades persistidas, usa tipo+Id para reduzir colisões entre tipos.
+        // Para transientes, fallback ao hash de referência evita comportamento inesperado.
+        return Id == Guid.Empty ? base.GetHashCode() : HashCode.Combine(GetType(), Id);
+    }
+
+    public static bool operator ==(Entity? left, Entity? right) =>
+        left is null ? right is null : left.Equals(right);
+
+    public static bool operator !=(Entity? left, Entity? right) => !(left == right);
+}
